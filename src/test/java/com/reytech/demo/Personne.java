7d2014bfd351d4a11193092c7bf38b15889bd2cf
zaba221 <36647938+zaba221@users.noreply.github.com>
2020-09-16 20:09:29 +0200

    Create Personne.java

---
+++ src/test/java/com/reytech/demo/Personne.java
@@ -0,0 +1,28 @@
+package com.reytech.demo;
+
+
+public class Personne {
+
+    public String firstName;
+    public String lastName;
+
+    public Personne(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
