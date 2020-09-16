9c47052a69db11dfb916be8dc969d101726c3355
zaba221 <36647938+zaba221@users.noreply.github.com>
2020-09-16 20:08:16 +0200

    creation test s assertions

---
+++ src/test/java/com/reytech/demo/StandardAssertionsTest.java
@@ -0,0 +1,38 @@
+package com.reyetech.demo;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.formation.model.Personne;
+
+class StandardAssertionsTest {
+
+	@DisplayName("Tests standards")
+    @Test
+    void standardAssertions() {
+    	
+        assertEquals(2, 2);
+        assertNotEquals(2, 3);
+        
+        // affiche le message si le test ne passe pas
+        assertTrue(true, "Un premier message.");
+        assertFalse(false, () ->  "Un " + "autre " + "message" + "." );  
+    }
+    
+    
+    @Test
+    void groupedAssertions() {
+        Personne pers = new Personne("Nassur", "Moumadi");
+
+        assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
+                              () -> assertEquals("Moumadii", pers.getLastName()));
+    }
+
+
+}
