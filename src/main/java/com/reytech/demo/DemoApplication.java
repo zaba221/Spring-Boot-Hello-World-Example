20cfd47ac44775397915270d2de7b3727051acd6
zaba221 <36647938+zaba221@users.noreply.github.com>
2020-09-23 09:20:39 +0200

    Update DemoApplication.java

--- src/main/java/com/reytech/demo/DemoApplication.java
+++ src/main/java/com/reytech/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	
-	private String nom = 12;
+	private String nom = "12";
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
